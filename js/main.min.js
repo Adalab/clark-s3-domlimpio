7c596d4f0a1a7cb12282cb6ff00a98f293835cbf
Rita Cascante <margalida.kaskante@gmail.com>
2018-01-16 16:28:10 +0100

    Last push autoindent

--- js/main.min.js
+++ js/main.min.js
@@ -33,28 +33,28 @@ init();
 //CONTADORES
 
 var initialNumber = 0;
-  var elementOne = document.querySelector('.stat-number-one');
-  var elementTwo = document.querySelector('.stat-number-two-desktop');
-  var elementThree = document.querySelector('.stat-number-three-desktop');
-  var maxValues = [25,97,60];
-  var elements = [elementOne,elementTwo,elementThree];
+var elementOne = document.querySelector('.stat-number-one');
+var elementTwo = document.querySelector('.stat-number-two-desktop');
+var elementThree = document.querySelector('.stat-number-three-desktop');
+var maxValues = [25,97,60];
+var elements = [elementOne,elementTwo,elementThree];
 
 function counterElement(maxValue,element) {
-    var maximum = Math.max.apply(null, maxValue);
-    var counterInterval = window.setInterval(function countNumber(){
-      var counter = initialNumber++;
+  var maximum = Math.max.apply(null, maxValue);
+  var counterInterval = window.setInterval(function countNumber(){
+    var counter = initialNumber++;
 
-      for (var i = 0; i < element.length; i++) {
-        if (counter <= maxValue[i]) {
-          element[i].innerHTML = counter;
-        }
+    for (var i = 0; i < element.length; i++) {
+      if (counter <= maxValue[i]) {
+        element[i].innerHTML = counter;
       }
-      if (counter > maximum) {
-        clearInterval(counterInterval);
-      }
+    }
+    if (counter > maximum) {
+      clearInterval(counterInterval);
+    }
   },80);
-    return counterInterval;
-  }
+  return counterInterval;
+}
 
 counterElement(maxValues,elements);
 
